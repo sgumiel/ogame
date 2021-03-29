d4dd0df1f7d63aab67a17f0cdd9d7885c6ab0c59
Sergio Gumiel <sgumiel@stratio.com>
2021-03-29 10:54:31 +0200

    Ogame inital commit

---
+++ src/main/java/com/gumi/ogame/OgameApplication.java
@@ -0,0 +1,13 @@
+package com.gumi.ogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gumi/ogame/OgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumi.ogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
